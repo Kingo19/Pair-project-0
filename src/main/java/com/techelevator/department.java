18029094a595241bafb16fd43acc64b7df20149b
Nigus Gebremariam <kingsgm19@gmail.com>
2023-09-26 13:35:47 -0400

    pair week 2

---
+++ src/main/java/com/techelevator/department.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+public class department {
+
+    private String name;
+    private int departmentID;
+
+    public department(String name, int departmentID) {
+
+
+
+
+    }
+}
---
+++ src/main/java/com/techelevator/project.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+public class project {
+    private String name;
+    private String description;
+    private String startDate;
+    private String dueDate;
+    private employee teamMembers;
+
+    public project(String name, String description, String startDate) {
+
+    }
+
+}
