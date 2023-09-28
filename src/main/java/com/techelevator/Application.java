8f2a0d81b5b1ed477ff8f5eae5ae7f2d8fcd7c30
Nigus Gebremariam <kingsgm19@gmail.com>
2023-09-27 21:52:38 -0400

    PAIR WEEK 2 - DONE

--- src/main/java/com/techelevator/Application.java
+++ src/main/java/com/techelevator/Application.java
@@ -33,79 +33,41 @@ public class Application {
     private void run() {
         // create some departments
         createDepartments(); {
-            departments.add(new Department(1, "Marketing"));
-            departments.add(new Department(2, "Sales"));
-            departments.add(new Department(3, "Engineering"));
+
         }
 
 
         // print each department by name
         printDepartments();  {
 
-        System.out.println("----------------------------DEPARTMENTS----------------------------------");
-        for (Department department : departments) {
-                System.out.println(department.getName());
-            }
+
         }
 
 
         // create employees
         createEmployees(); {
 
-                Department engineeringDepartment = departments.get(2); // Get Engineering department
-                Department marketingDepartment = departments.get(0);  // Get Marketing department
 
-                employees.add(new Employee(1, "Steve", "Carmicael", "steve@teams.com", engineeringDepartment, "08/21/2020"));
-                employees.add(new Employee(2, "Chala", "Mosisa", "chala@teams.com", engineeringDepartment, "08/21/2020"));
-                employees.add(new Employee(3, "Nigus", "Gebremariam", "nigus@teams.com", marketingDepartment, "08/21/2020"));
-
-            // give Chala a 10% raise, she is doing a great job!
-            Employee Chala = employees.get(1);
-            Chala.raiseSalary(10);
         }
 
 
         // print all employees
         printEmployees(); {
-            System.out.println("------------- EMPLOYEES ------------------------------");
-            for (Employee employee : employees) {
-                System.out.println(employee.getFullName() + " (" + employee.getSalary() + ") " + employee.getDepartment().getName());
-            }
+
         }
 
         // create the TEams project
         createTeamsProject(); {
-            Project teamsProject = new Project("Teams", "Project Management Software", "10/10/2020", "11/10/2020");
 
-            for (Employee employee : employees) {
-                if (employee.getDepartment().getName().equals("Engineering")) {
-                    teamsProject.getTeamMembers().add(employee);
-                }
-            }
-
-            projects.put(teamsProject.getName(), teamsProject);
         }
         // create the Marketing Landing Page Project
         createLandingPageProject(); {
-            Project landingPageProject = new Project("Marketing Landing Page", "Lead Capture Landing Page for Marketing", "10/10/2020", "10/17/2020");
 
-            for (Employee employee : employees) {
-                if (employee.getDepartment().getName().equals("Marketing")) {
-                    landingPageProject.getTeamMembers().add(employee);
-                }
-            }
-
-            projects.put(landingPageProject.getName(), landingPageProject);
         }
 
         // print each project name and the total number of employees on the project
         printProjectsReport(); {
-            System.out.println("------------- PROJECTS ------------------------------");
-            for (Map.Entry<String, Project> entry : projects.entrySet()) {
-                String projectName = entry.getKey();
-                int teamSize = entry.getValue().getTeamMembers().size();
-                System.out.println(projectName + ": " + teamSize);
-            }
+
         }
     }
 
@@ -113,6 +75,9 @@ public class Application {
      * Create departments and add them to the collection of departments
      */
     private void createDepartments() {
+        departments.add(new Department(1, "Marketing"));
+        departments.add(new Department(2, "Sales"));
+        departments.add(new Department(3, "Engineering"));
 
     }
 
@@ -120,6 +85,10 @@ public class Application {
      * Print out each department in the collection.
      */
     private void printDepartments() {
+        System.out.println("----------------------------DEPARTMENTS----------------------------------");
+        for (Department department : departments) {
+            System.out.println(department.getName());
+        }
 
 
     }
@@ -128,6 +97,16 @@ public class Application {
      * Create employees and add them to the collection of employees
      */
     private void createEmployees() {
+        Department engineeringDepartment = departments.get(2); // Get Engineering department
+        Department marketingDepartment = departments.get(0);  // Get Marketing department
+
+        employees.add(new Employee(1, "Steve", "Carmicael", "steve@teams.com", engineeringDepartment, "08/21/2020"));
+        employees.add(new Employee(2, "Chala", "Mosisa", "chala@teams.com", engineeringDepartment, "08/21/2020"));
+        employees.add(new Employee(3, "Nigus", "Gebremariam", "nigus@teams.com", marketingDepartment, "08/21/2020"));
+
+        // give Chala a 10% raise, she is doing a great job!
+        Employee Chala = employees.get(1);
+        Chala.raiseSalary(10);
 
 
     }
@@ -136,6 +115,10 @@ public class Application {
      * Print out each employee in the collection.
      */
     private void printEmployees() {
+        System.out.println("------------- EMPLOYEES ------------------------------");
+        for (Employee employee : employees) {
+            System.out.println(employee.getFullName() + " (" + employee.getSalary() + ") " + employee.getDepartment().getName());
+        }
 
 
         }
@@ -146,6 +129,15 @@ public class Application {
      * Create the 'TEams' project.
      */
     private void createTeamsProject() {
+        Project teamsProject = new Project("Teams", "Project Management Software", "10/10/2020", "11/10/2020");
+
+        for (Employee employee : employees) {
+            if (employee.getDepartment().getName().equals("Engineering")) {
+                teamsProject.getTeamMembers().add(employee);
+            }
+        }
+
+        projects.put(teamsProject.getName(), teamsProject);
 
 
     }
@@ -154,6 +146,15 @@ public class Application {
      * Create the 'Marketing Landing Page' project.
      */
     private void createLandingPageProject() {
+        Project landingPageProject = new Project("Marketing Landing Page", "Lead Capture Landing Page for Marketing", "10/10/2020", "10/17/2020");
+
+        for (Employee employee : employees) {
+            if (employee.getDepartment().getName().equals("Marketing")) {
+                landingPageProject.getTeamMembers().add(employee);
+            }
+        }
+
+        projects.put(landingPageProject.getName(), landingPageProject);
 
 
     }
@@ -162,6 +163,12 @@ public class Application {
      * Print out each project in the collection.
      */
     private void printProjectsReport() {
+        System.out.println("------------- PROJECTS ------------------------------");
+        for (Map.Entry<String, Project> entry : projects.entrySet()) {
+            String projectName = entry.getKey();
+            int teamSize = entry.getValue().getTeamMembers().size();
+            System.out.println(projectName + ": " + teamSize);
+        }
 
 
     }
